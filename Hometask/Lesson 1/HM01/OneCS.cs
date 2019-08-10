6c3af6432146392659a68f1c012ce37a566dada3
therealadik <53974452+therealadik@users.noreply.github.com>
2019-08-10 23:17:44 +0500

    Create OneCS.cs

---
+++ Hometask/Lesson 1/HM01/OneCS.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneSC : MonoBehaviour
+{
+    public Vector3 pos0 = new Vector3(0f,0f,0f);
+    void Start()
+    {
+      transform.position = pos0;  
+    }
+    void Update()
+    {
+       
+    }
+}
